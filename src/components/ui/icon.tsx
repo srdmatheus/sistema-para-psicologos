3ca1712998e9b85ef163222f9d44910cced53ab8
Matheus Ribeiro <srdmatheus@gmail.com>
2024-04-23 17:09:46 -0300

    feat: add icon component

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,23 @@
+import {
+  CalendarDaysIcon,
+  LayoutGridIcon,
+  LucideProps,
+  type LucideIcon
+} from "lucide-react";
+
+export const Icon = {
+  calendar: CalendarDaysIcon,
+  grid: LayoutGridIcon
+};
+
+export type IconType = LucideIcon;
+export type IconName = keyof typeof Icon;
+
+interface DynamicIconProps extends LucideProps {
+  name: IconName;
+}
+
+export function DynamicIcon({ name, ...props }: DynamicIconProps) {
+  const IconComponent = Icon[name];
+  return <IconComponent {...props} />;
+}
