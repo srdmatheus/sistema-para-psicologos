f98d584551c4ac8c0b4aac44d0a877b09a06dbbc
Matheus Ribeiro <srdmatheus@gmail.com>
2024-04-24 11:55:49 -0300

    chore: add manrope font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+
+import { manrope } from '@/fonts';
 
 import '@/styles/globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Sistema para psicólogos',
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.variable}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontFamily: {
+      manrope: ['var(--font-manrope)']
+    },
     extend: {
       colors: {
         background: {
