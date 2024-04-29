a8705f9abf196bfd82c054591d4ef6a1626e0921
Matheus Ribeiro <srdmatheus@gmail.com>
2024-04-28 23:30:02 -0300

    feat: add customer page

--- src/app/(admin)/clientes/page.tsx
+++ src/app/(admin)/clientes/page.tsx
@@ -1,14 +1,11 @@
 import { getCustomers } from '@/actions/get-customers';
-
-import { CreateCustomerDialog } from '@/components/app/';
-import { CustomerList } from '@/components/app/customer-list';
-import { Icon } from '@/components/ui';
+import { CreateCustomerDialog, CustomerList, Icon } from '@/components';
 
 export default async function PatientsPage() {
   const customers = await getCustomers();
   return (
     <section className="p-8">
-      <h1 className="text-3xl font-extrabold tracking-tight">Pacientes</h1>
+      <h1 className="text-3xl font-extrabold tracking-tight">Clientes</h1>
       <div className="mt-8 flex gap-4">
         <div className="flex w-96 items-center gap-2 rounded-xl border bg-background-foreground p-2 ring-primary focus-within:ring-2">
           <Icon.search className="size-4" />
