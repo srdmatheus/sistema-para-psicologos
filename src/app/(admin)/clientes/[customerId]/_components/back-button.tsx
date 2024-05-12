c40c043f0cb144df7a87c91c186fd354cfb3d3a6
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-12 13:03:33 -0300

    feat: add tooltip

--- src/app/(admin)/clientes/[customerId]/_components/back-button.tsx
+++ src/app/(admin)/clientes/[customerId]/_components/back-button.tsx
@@ -2,13 +2,26 @@
 
 import { useRouter } from 'next/navigation';
 
-import { Button, Icon } from '@/_components';
+import { Button, Icon, Tooltip } from '@/_components';
 
 export const BackButton = () => {
   const router = useRouter();
   return (
-    <Button onClick={() => router.back()} variant="outline">
-      <Icon.chevronLeft size={14} /> Voltar
-    </Button>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <Button
+            onClick={() => router.back()}
+            variant="outline"
+            className="h-10 w-10 p-0"
+          >
+            <Icon.chevronLeft size={14} />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content>
+          <p>Voltar</p>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
