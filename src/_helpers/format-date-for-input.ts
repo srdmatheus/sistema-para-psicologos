e571069f30d1b4849f40145b55d31e14718c3967
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-12 09:56:58 -0300

    chore: add utils

---
+++ src/_helpers/format-date-for-input.ts
@@ -0,0 +1,9 @@
+export const formatDateForInput = (date: Date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
+
+  return formattedDate;
+};
