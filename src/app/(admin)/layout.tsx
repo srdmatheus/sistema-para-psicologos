169436cc17ae99dd52e0194e37b7ab9b78054ce8
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-14 23:44:58 -0300

    refactor: change mobile styles

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,9 +4,9 @@ import { Sidebar } from '@/_components';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex h-full">
+    <main className="relative flex h-full">
       <Sidebar />
-      {children}
+      <div className="ml-0 w-full xs:ml-[4.25rem] md:ml-64">{children}</div>
     </main>
   );
 }
