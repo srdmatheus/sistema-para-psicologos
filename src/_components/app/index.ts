6dcc05320c886bec109e184fecd7db816defb942
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-14 23:41:38 -0300

    feat: add table component

--- src/_components/app/index.ts
+++ src/_components/app/index.ts
@@ -2,5 +2,4 @@ export * from './sidebar/sidebar';
 export * from './create-consultation-dialog';
 export * from './create-customer-dialog';
 export * from './create-note-dialog';
-export * from './customer-list';
 export * from './update-customer-dialog';
