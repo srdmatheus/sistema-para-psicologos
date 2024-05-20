67f09c8a52500b45b4eb23133b810c79ee1d88cc
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-19 22:55:41 -0300

    feat: add sonner component

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from 'react';
 
 import { Sidebar } from '@/_components';
+import { Toaster } from '@/_components/ui/sonner';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <main className="relative flex h-full">
       <Sidebar />
       <div className="ml-0 w-full xs:ml-[4.25rem] md:ml-64">{children}</div>
+      <Toaster />
     </main>
   );
 }
