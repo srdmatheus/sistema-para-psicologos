d16278fb228ae664bf2b9b2b80333a381111d7ce
Matheus Ribeiro <srdmatheus@gmail.com>
2024-05-23 23:59:30 -0300

    chore: verify if user is authenticated

--- src/_actions/create-consultation.tsx
+++ src/_actions/create-consultation.tsx
@@ -3,13 +3,25 @@
 import { revalidatePath } from 'next/cache';
 
 import { db } from '@/_lib/prisma';
+import { auth } from '@/auth';
 import { Prisma } from '@prisma/client';
 
 export const createConsultation = async (
-  data: Prisma.ConsultationCreateInput
+  data: Prisma.ConsultationUncheckedCreateInput
 ) => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error('Usuário não autenticado');
+  }
+
+  const consultationData: Prisma.ConsultationUncheckedCreateInput = {
+    ...data,
+    customerId: data.customerId
+  };
   await db.consultation.create({
-    data
+    data: {
+      ...consultationData
+    }
   });
 
   revalidatePath('clientes');
